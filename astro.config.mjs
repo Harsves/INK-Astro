05f1a413b4d9b9dfe5fa2d048817ee85f235c1d3
Suuskehh <suus.harsveld@hva.nl>
2023-11-21 15:14:52 +0100

    Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,36 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from '@astrojs/tailwind'
-import basicSsl from '@vitejs/plugin-basic-ssl'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
+import { defineConfig } from 'astro/config';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
+import tailwind from '@astrojs/tailwind';
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import netlify from "@astrojs/netlify/functions";
+const env = loadEnv('', process.cwd(), 'STORYBLOK');
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
-      // accessToken: 'VDsIqgdaKldvjOOQa18nmwtt',
-      apiOptions: {
-        region: '',
-      },
-      components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        hero: 'storyblok/Hero',
-        material: 'storyblok/Material',
-        financial: 'storyblok/Financial',
-        clothing: 'storyblok/Clothing',
-      },
-    }),
-    tailwind(),
-  ],
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    // accessToken: 'VDsIqgdaKldvjOOQa18nmwtt',
+    apiOptions: {
+      region: ''
+    },
+    components: {
+      page: 'storyblok/Page',
+      feature: 'storyblok/Feature',
+      grid: 'storyblok/Grid',
+      hero: 'storyblok/Hero',
+      material: 'storyblok/Material',
+      financial: 'storyblok/Financial',
+      clothing: 'storyblok/Clothing'
+    }
+  }), tailwind()],
   vite: {
     plugins: [basicSsl()],
     server: {
-      https: true,
-    },
+      https: true
+    }
   },
-})
+  output: "server",
+  adapter: netlify()
+});
