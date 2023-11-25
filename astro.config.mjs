bd7d31032edf085b2ec6a51aac59fa40ab8f8a25
Suuskehh <suus.harsveld@hva.nl>
2023-11-25 11:24:43 +0100

    update netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -33,3 +33,12 @@ export default defineConfig({
   output: "server",
   adapter: netlify()
 });
+
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify/functions';
+
+export default defineConfig({
+  output: "server",
+  adapter: netlify(),
+});
